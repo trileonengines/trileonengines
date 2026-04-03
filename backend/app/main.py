789514cdc8f590f2cb2ca330297ccbb03d004c42
trileonengines <1kpapicasino@gmail.com>
2026-04-02 23:20:39 -0400

    Initialize FastAPI application with engine router

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.routes.engine import router
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"Trileon": "alive"}
+
+app.include_router(router, prefix="/engine")
